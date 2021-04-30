27594fd8a19b790040ccfbd3ee4bebd6e4499d40
renan <renan.engcomp@gmail.com>
2021-04-30 16:52:16 -0300

    Add comments to fetch_data utils

--- utils/fetch_data.py
+++ utils/fetch_data.py
@@ -6,6 +6,10 @@ DOWNLOAD_ROOT = "DATA_ROOT_URL"
 DATA_PATH = os.path.join("datasets", "data") # where data will be stored in your machine
 DATA_URL = DOWNLOAD_ROOT + "PATH_TO_DATA"
 
+"""
+It creates a datasets/data directory in your workspace, downloads a data.tgz file,
+and extracts the contents from it in this directory
+"""
 def fetch_data(data_url=DATA_URL, data_path=DATA_PATH):
     if not os.path.isdir(data_path):
         os.makedirs(data_path)
