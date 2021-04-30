dc637a79236bc011ada26756057273b48d35ef97
renan <renan.engcomp@gmail.com>
2021-04-30 16:47:18 -0300

    Add fetch_data utils

---
+++ utils/fetch_data.py
@@ -0,0 +1,16 @@
+import os
+import tarfile
+import urllib.request
+
+DOWNLOAD_ROOT = "DATA_ROOT_URL"
+DATA_PATH = os.path.join("datasets", "data") # where data will be stored in your machine
+DATA_URL = DOWNLOAD_ROOT + "PATH_TO_DATA"
+
+def fetch_data(data_url=DATA_URL, data_path=DATA_PATH):
+    if not os.path.isdir(data_path):
+        os.makedirs(data_path)
+    tgz_path = os.path.join(data_path, "data.tgz")
+    urllib.request.urlretrieve(data_url, tgz_path)
+    data_tgz = tarfile.open(tgz_path)
+    data_tgz.extractall(path=data_path)
+    data_tgz.close()
