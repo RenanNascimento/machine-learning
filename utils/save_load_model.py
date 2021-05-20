e8f2c00cff20ca38af7467d5570501ee1b1ca043
renan <renan.engcomp@gmail.com>
2021-05-20 17:39:32 -0300

    Update save_load_model file format

--- utils/save_load_model.py
+++ utils/save_load_model.py
@@ -1,5 +1,5 @@
 import joblib
 
-joblib.dump(my_model, "my_model.pkl")
+joblib.dump(my_model, "my_model.joblib")
 #...
-my_model_loaded = joblib.load("my_model.pkl")
+my_model_loaded = joblib.load("my_model.joblib")
